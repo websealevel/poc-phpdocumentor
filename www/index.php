3a0e62c61733acccb2a4e8e3105492f5fc658355
paul <paul@genesis.websealevel.com*>
2022-04-01 03:47:10 +0200

    first commit

---
+++ www/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This is a Summary.
+ *
+ * @see Markdown
+ *
+ * @param int        $parameter1 A parameter description.
+ * @param \Exception $parameter2 Another parameter description.
+ *
+ * @\Doctrine\Orm\Mapper\Entity()
+ *
+ * @return string
+ */
+function test($parameter1, $parameter2)
+{
+}
